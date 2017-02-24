931e27eb5515fc6ee492e4c6a57a0ade730484bf
Javier Vidal <jvpenya@gmail.com>
2017-02-24 18:22:48 +0100

    Removed following menu from tabbar

--- resources/views/master/tabbar.blade.php
+++ resources/views/master/tabbar.blade.php
@@ -4,8 +4,10 @@
             <div class="item">
                 <img class="ui mini circular image" src="{{ Auth::user()->avatar }}">
             </div>
-            <a  href="/home" class="item">{{ Auth::user()->name }}</a>
-            <a  href="/follow" class="item">Following</a>
+            <a  href="/home" class="item">{{ Auth::user()->nickname }}</a>
+            @if (Auth::user()->following()->count() > 0)
+              <a  href="/follow" class="item">Following</a>
+            @endif
             <div class="right menu">
                 <div class="item">
                     <form action="/logout" class="ui form" method="post">
