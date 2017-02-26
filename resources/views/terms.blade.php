2214ab783b4736e044b7a1eb878b947d043ea78b
Javier Vidal <jvpenya@gmail.com>
2017-02-26 14:34:10 +0100

    Updated terms and conditions

--- resources/views/terms.blade.php
+++ resources/views/terms.blade.php
@@ -24,30 +24,45 @@
     <li>This license shall automatically terminate if you violate any of these restrictions and may be terminated by {{ config('app.name') }} at any time. Upon terminating your viewing of these materials or upon the termination of this license, you must destroy any downloaded materials in your possession whether in electronic or printed format.</li>
   </ol>
 
-<h3>3. Disclaimer</h3>
+  <h3>3. Payments</h3>
+  <p>Payment processing services for premium accounts on {{ config('app.name') }} are provided by Stripe and are
+      subject to the <a href="https://stripe.com/connect-account/legal">
+          Stripe Connected Account Agreement</a>, which includes the
+          <a href="https://stripe.com/legal">Stripe Terms of Service</a>
+          (collectively, the “Stripe Services Agreement”). By agreeing to
+          these terms on {{ config('app.name') }}, you agree to be bound
+          by the Stripe Services Agreement, as the same may be modified by
+          Stripe from time to time. As a condition of {{ config('app.name') }}
+          enabling payment processing services through Stripe, you agree to
+          provide {{ config('app.name') }} accurate and complete information about
+          you and your business, and you authorize {{ config('app.name') }} to share
+          it and transaction information related to your use of the payment
+          processing services provided by Stripe.
+      </p>
+<h3>4. Disclaimer</h3>
 
   <ol type="a">
     <li>The materials on {{ config('app.name') }}'s website are provided on an 'as is' basis. {{ config('app.name') }} makes no warranties, expressed or implied, and hereby disclaims and negates all other warranties including, without limitation, implied warranties or conditions of merchantability, fitness for a particular purpose, or non-infringement of intellectual property or other violation of rights.</li>
     <li>Further, {{ config('app.name') }} does not warrant or make any representations concerning the accuracy, likely results, or reliability of the use of the materials on its website or otherwise relating to such materials or on any sites linked to this site.</li>
   </ol>
 
-<h3>4. Limitations</h3>
+<h3>5. Limitations</h3>
 
   <p>In no event shall {{ config('app.name') }} or its suppliers be liable for any damages (including, without limitation, damages for loss of data or profit, or due to business interruption) arising out of the use or inability to use the materials on {{ config('app.name') }}'s website, even if {{ config('app.name') }} or a {{ config('app.name') }} authorized representative has been notified orally or in writing of the possibility of such damage. Because some jurisdictions do not allow limitations on implied warranties, or limitations of liability for consequential or incidental damages, these limitations may not apply to you.</p>
 
-<h3>5. Accuracy of materials</h3>
+<h3>6. Accuracy of materials</h3>
 
   <p>The materials appearing on {{ config('app.name') }}'s website could include technical, typographical, or photographic errors. {{ config('app.name') }} does not warrant that any of the materials on its website are accurate, complete or current. {{ config('app.name') }} may make changes to the materials contained on its website at any time without notice. However {{ config('app.name') }} does not make any commitment to update the materials.</p>
 
-<h3>6. Links</h3>
+<h3>7. Links</h3>
 
   <p>{{ config('app.name') }} has not reviewed all of the sites linked to its website and is not responsible for the contents of any such linked site. The inclusion of any link does not imply endorsement by {{ config('app.name') }} of the site. Use of any such linked website is at the user's own risk.</p>
 
-<h3>7. Modifications</h3>
+<h3>8. Modifications</h3>
 
   <p>{{ config('app.name') }} may revise these terms of service for its website at any time without notice. By using this website you are agreeing to be bound by the then current version of these terms of service.</p>
 
-<h3>8. Governing Law</h3>
+<h3>9. Governing Law</h3>
 
   <p>These terms and conditions are governed by and construed in accordance with the laws of Spain and you irrevocably submit to the exclusive jurisdiction of the courts in that location.</p>
 
